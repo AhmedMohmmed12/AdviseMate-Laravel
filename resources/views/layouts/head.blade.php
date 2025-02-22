621c793d2863a4f2456e16b1a26f59d3deedb76e
Ahmed <93286895+AhmedMohmmed12@users.noreply.github.com>
2025-02-22 19:28:27 +0300

    adding heading layout

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+
+    
+    @if (app()->getLocale() == 'en')
+    <link rel="stylesheet" href="{{asset('/css/styles.css')}}">
+    @else   
+    <link rel="stylesheet" href="{{asset('/css/styles.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/style-ar.css')}}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap" rel="stylesheet">
+    @endif
+</head>
+<body>
+    @include('components.sidebar')
+</body>
+</html>
